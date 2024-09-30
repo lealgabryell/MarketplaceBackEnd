1b0210c90da759f9177f3634b32b4d157b5ee12f
Francinaldo Batista <140225853+Batissta@users.noreply.github.com>
2024-09-29 21:13:50 -0300

    Update cliente.js

--- src/models/cliente.js
+++ src/models/cliente.js
@@ -6,7 +6,7 @@ const clienteSchema = new mongoose.Schema({
   telefone: { type: String, required: true },
   email: { type: String, required: true, unique: true },
   senha: { type: String, required: true },
-  dataNascimento: { type: Date, required: true },
+  dataNascimento: { type: String, required: true },
   cpf: { type: String, required: true, unique: true },
   dataRegistro: { type: Date, default: Date.now },
   desconto: { type: Boolean, default: false },
